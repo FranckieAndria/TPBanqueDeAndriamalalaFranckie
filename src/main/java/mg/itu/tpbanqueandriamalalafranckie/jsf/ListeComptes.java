d80c2dd146461b4e671a007dfc2b928252711b2d
FranckieAndria <franckieandriamalala@gmail.com>
2024-04-11 20:36:21 +0300

    Création de la page ListeComptes qui liste tous les comptes

---
+++ src/main/java/mg/itu/tpbanqueandriamalalafranckie/jsf/ListeComptes.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.itu.tpbanqueandriamalalafranckie.jsf;
+
+import jakarta.inject.Named;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import java.io.Serializable;
+import java.util.List;
+import mg.itu.tpbanqueandriamalalafranckie.entity.CompteBancaire;
+import mg.itu.tpbanqueandriamalalafranckie.service.GestionnaireCompte;
+
+/**
+ * Backing bean pour la page JSF listeComptes
+ *
+ * @author andriamalala
+ */
+@Named(value = "listeComptes")
+@ViewScoped
+public class ListeComptes implements Serializable {
+
+    @Inject
+    private GestionnaireCompte gestionCompte;
+
+    private List<CompteBancaire> compteBancaireList;
+
+    /**
+     * Retourne la liste de tous les comptes
+     *
+     * @return
+     */
+    public List<CompteBancaire> getAllComptes() {
+        if (this.compteBancaireList == null) {
+            this.compteBancaireList = this.gestionCompte.getAllComptes();
+        }
+        return this.compteBancaireList;
+    }
+
+    /**
+     * Creates a new instance of ListeComptes
+     */
+    public ListeComptes() {
+    }
+
+}
